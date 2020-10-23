62be1cc86f7a7284dc23c77a985a47f1900f7d40
Jenner Nicodemos <jenner.nicodemos@gmail.com>
2020-10-23 16:44:43 -0300

    Recebendo e validando os dados de formulário de entrada

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,17 @@
 module.exports.startChat = function(application, req, res) {
+
+    let dataForm = req.body
+
+    req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty()
+    req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15)
+    
+    let error = req.validationErrors()
+
+    if (error) {
+        res.render('index', {validation: error})
+        return
+    }
+    
+    
     res.render('chat')
 }
